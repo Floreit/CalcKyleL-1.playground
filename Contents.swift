7b9646d70d13b7a6f0ed38e8fa8ffa145276f302
Floreit <kludford@e116-m-21.mchenry.edu>
2017-02-17 10:01:48 -0600

    calc
    calc

---
+++ Contents.swift
@@ -0,0 +1,27 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+// choose the numbers you need to use
+var one = 100
+var two = 25
+
+// Everything bellow just shows the possible outcomes for the 4 basic math, change one and two to get the numbers you want to do math with
+var addition = one + two
+var subtraction = one - two
+var mult = one * two
+var divide = one / two
+var per = two % one
+
+
+
+// Answers
+print(addition)
+print(subtraction)
+print(mult)
+print(divide)
+print (per)
+
+
+
+
+
